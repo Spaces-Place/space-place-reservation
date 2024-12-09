b887ae844fcfe4c3419996f842c1e7e03e5b5064
DESKTOP-LRDGUCJ\TAEMIN <kw3135@gmail.com>
2024-12-09 09:31:48 +0900

    - 운영 환경 카프카 설정 코드 추가(오류 가능성 있음)

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from fastapi import Depends, FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from prometheus_fastapi_instrumentator import Instrumentator
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.semconv.resource import ResourceAttributes
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 
 from routers.reservation import reservation_router
 from utils.database_config import DatabaseConfig
@@ -56,10 +62,21 @@ async def health_check(logger: Logger = Depends(Logger.setup_logger)) -> dict:
     return {"status": "ok"}
 
 
-FastAPIInstrumentor.instrument_app(app)
+"""Trace"""
+# OpenTelemetry
+resource = Resource.create({ResourceAttributes.SERVICE_NAME: "reservation-service"})
+trace_provider = TracerProvider(resource=resource)
 
+# 템포에 데이터 전송을 위한 OLTP span Exporter
+tempo_exporter = OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces")
+span_processor = BatchSpanProcessor(tempo_exporter)
+trace_provider.add_span_processor(span_processor) # Span 프로세서 추가
+
+trace.set_tracer_provider(trace_provider)
+
+FastAPIInstrumentor.instrument_app(app, excluded_urls="client/.*/health")
 instrumentator = Instrumentator()
-instrumentator.instrument(app).expose(app)
+instrumentator.instrument(app).expose(app) # 메트릭(/metrics) 노출
 
 
 app.add_middleware(
